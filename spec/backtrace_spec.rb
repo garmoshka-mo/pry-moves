e2808284313e07a7b61bc3c908ee9dda5bb799b7
Daniel <dan@coav.ru>
2021-03-22 19:03:04 +0200

    fix backtrace spec

--- spec/backtrace_spec.rb
+++ spec/backtrace_spec.rb
@@ -7,12 +7,13 @@ describe 'backtrace' do
       [nil, 'stop in level_c'],
       ['bt', lambda{|b, output|
         lines = output.split("\n").reverse
-        expect(lines[0]).to end_with 'level_c(param=?: nil)'
-        expect(lines[1]).to end_with 'level_a()'
-        expect(lines[2]).to include 'Playground:'
-        expect(lines[3]).to end_with ':block'
-        expect(lines[4]).to include 'RSpec::ExampleGroups'
-        expect(lines.count).to be 5
+        expect(lines[0]).to end_with 'level_c(param=? nil)'
+        expect(lines[1]).to end_with 'frames hidden: 1'
+        expect(lines[2]).to end_with 'level_a()'
+        expect(lines[3]).to include 'Playground'
+        expect(lines[4]).to end_with ':block'
+        expect(lines[5]).to include 'RSpec::ExampleGroups'
+        expect(lines.count).to be 7
       }],
       ['bt hidden', lambda{|b, output|
         lines = output.split("\n").reverse
@@ -20,7 +21,7 @@ describe 'backtrace' do
         expect(lines[1]).to end_with 'level_b()'
         expect(lines.count).to be 11
       }],
-      ['bt 2', lambda{|b, output|
+      ['bt 33', lambda{|b, output|
         lines = output.split("\n").reverse
         expect(lines[1]).to end_with 'level_b # inside of level_a'
       }],
