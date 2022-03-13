ab90e949ec536730bc9e8a635505175241f3c840
Daniel <d@coav.ru>
2022-03-13 19:31:55 +0200

    tune playground

--- playground/sand.rb
+++ playground/sand.rb
@@ -1,3 +1,6 @@
+# cd playground
+# be ruby sand.rb
+
 require 'pry-moves'
 require './tracer.rb'
 
@@ -7,42 +10,41 @@ def fi(param)
   puts param
 end
 
-class A
+class Sand
 
   def initialize
     puts :xuilo
   end
 
-  def aa
+  def some_logic
     puts 'aa: step 1'
     puts 'aa: step 2'
   end
 
-  def bb
+  def method_with_hidden_stack
     debug_redirect = :aa
     hide_from_stack = true
     puts 'bb: step 1'
     puts 'bb: step 2'
-    aa
+    some_logic
   end
 
-  def cc
+  def debugged_method
     koko = :love
-    binding.pry
-    bb
+    debug
+    method_with_hidden_stack
     (2..4).each do |i|
       puts i
     end
     puts :two
   end
-  alias cc_al cc
+  alias debugged_method_alias debugged_method
 
 end
 
 puts :prepare
 
-A.new.cc_al
-A.new.cc_al
+Sand.new.method_with_hidden_stack
 
 
 bb = 1
