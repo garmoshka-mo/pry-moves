002418bbd09e566984547040589cc77667842849
Daniel <d@coav.ru>
2022-09-22 17:48:05 +0200

    fix ident

--- lib/commands/next.rb
+++ lib/commands/next.rb
@@ -17,22 +17,22 @@ class PryMoves::Next < PryMoves::TraceCommand
 
     return unless @call_depth == 0 and traced_method?(file, line, method, binding_)
 
-      if event == 'line'
-        if @stay_at_frame
-          return (
-            @stay_at_frame == current_frame_digest or
-            @c_stack_level < 0
-          )
-        elsif @start_line != line or (
-            @events_traced > 1 and # чтобы не застревало на while (vx = shift)
-            @start_digest == current_frame_digest # for correct iterating over one_line_in_block
-          )
-          return true
-        end
+    if event == 'line'
+      if @stay_at_frame
+        return (
+          @stay_at_frame == current_frame_digest or
+          @c_stack_level < 0
+        )
+      elsif @start_line != line or (
+          @events_traced > 1 and # чтобы не застревало на while (vx = shift)
+          @start_digest == current_frame_digest # for correct iterating over one_line_in_block
+        )
+        return true
       end
+    end
 
-      true if event == 'return' and
-        method == @method[:name] and @method.before_end?(line)
+    true if event == 'return' and
+      method == @method[:name] and @method.before_end?(line)
   end
 
   def traced_method?(file, line, id, binding_)
