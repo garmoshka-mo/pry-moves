8bf920be068b5ce225f38416ad5f5ca9d174a8d5
Daniel <d@coav.ru>
2023-03-06 12:12:09 +0100

    disable next breakpoint

--- lib/commands/next_breakpoint.rb
+++ lib/commands/next_breakpoint.rb
@@ -4,6 +4,10 @@ class PryMoves::NextBreakpoint < PryMoves::TraceCommand
   end
 
   def trace(event, file, line, method, binding_)
+    # this command will stuck in tracing when no breakpoint met (usually)
+    PryMoves.messages << "⚠️  Command NextBreakpoint is not implemented"
+    return true
+
     if binding_.local_variable_defined?(:pry_breakpoint) and
         binding_.local_variable_get(:pry_breakpoint)
       binding_.local_variable_set :pry_breakpoint, nil # reset breakpoint at visited method instance
